c210f68d7ab6b4606b644a4f2bafc630d2fffde3
Egorov E.A <emelya.kara@yandex.ru>
2023-06-11 17:33:00 +0500

    реализован поиск, вывод не работает

--- backend/src/operations/router.py
+++ backend/src/operations/router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import func
+from sqlalchemy.orm import load_only
+from starlette.responses import JSONResponse
 
 from backend.src.database import get_async_session
 from backend.src.operations.models import names
@@ -17,13 +20,22 @@ router = APIRouter(
 
 
 @router.post("/")
-async def get_specific_operations(name: str, session: AsyncSession = Depends(get_async_session)):
+async def search_name(name: str, session: AsyncSession = Depends(get_async_session)):
     fixed_name = speller.spelled(name)
-    query = select(names).where(names.c.name.like(f"%{fixed_name}%"))
+    query = select(names.c.name).where(func.lower(names.c.name).like(func.lower(f"%{fixed_name}%")))
+    print(query)
     result = await session.execute(query)
     print(result.all())
-    return name
+    return JSONResponse({"text": result.all()})
 
+# @app.post("/search")
+# async def search(data = Body()):
+#
+#     request = data['request']
+#     fixed = speller.spelled(request)
+#     cursor.execute(f"select name from names where lower(name) like lower('%{fixed}%')")
+#     all_names = cursor.fetchall()
+#     return JSONResponse({"text": all_names})
 
 # @router.post("/")
 # async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
