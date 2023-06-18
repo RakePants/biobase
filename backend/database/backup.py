ce280939b7c13437e7fc654769056455b8a41136
Egorov E.A <emelya.kara@yandex.ru>
2023-06-19 00:34:29 +0500

    Добавить мелкую правку

--- backend/database/backup.py
+++ backend/database/backup.py
@@ -1,26 +1,27 @@
 import sys
 from os.path import dirname as up
 
-project_dir = up(up(up(__file__)))
-sys.path.append(project_dir)
-
 import yadisk
 import os
 import datetime
 from backend.src.config import YA_TOKEN
 
+project_dir = up(up(up(__file__)))
+sys.path.append(project_dir)
+
+
 def backup():
     dump_dir = os.getcwd() + '/dumps'
     filename = f"bio_{datetime.date.today().strftime('%d/%m/%Y')}.sql"
-    
+
     # TODO: переделать на сервере
     os.system(f"pg_dump bio > {dump_dir}/{filename}")
 
-    y = yadisk.YaDisk(token="YA_TOKEN")
+    y = yadisk.YaDisk(token=YA_TOKEN)
 
     if y.check_token():
         y.upload(f"{filename}", f"/backup/{filename}")
         print(f"Uploaded {filename}")
-        
+
     else:
         print('Token is invalid')
