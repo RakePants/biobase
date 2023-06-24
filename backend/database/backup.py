d77a16e70584874e6d605062d578efe969f3b736
Artyom Eryomkin <43377589+RakePants@users.noreply.github.com>
2023-06-24 20:31:49 +0500

    Fixed backup module

--- backend/database/backup.py
+++ backend/database/backup.py
@@ -1,26 +1,27 @@
 import sys
 from os.path import dirname as up
 
+project_dir = up(up(up(__file__)))
+sys.path.append(project_dir)
+
 import yadisk
 import os
 import datetime
 from backend.src.config import DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT, YA_TOKEN
 
-project_dir = up(up(up(__file__)))
-sys.path.append(project_dir)
-
 
 def backup():
-    dump_dir = os.getcwd() + '/dumps'
-    filename = f"{DB_NAME}_{datetime.date.today().strftime('%d/%m/%Y')}.sql"
-
-    # TODO: переделать на сервере
-    os.system(f"pg_dump {DB_NAME} > {dump_dir}/{filename}")
+    
+    dump_dir = os.getcwd() + '/dumps/'
+    filename = f"{DB_NAME}{datetime.date.today().strftime('%d:%m:%Y')}.dump"
+    print(dump_dir)
+    
+    os.system(f"sudo -i -u postgres pg_dump {DB_NAME} > {dump_dir}/{filename}")
 
     y = yadisk.YaDisk(token=YA_TOKEN)
 
     if y.check_token():
-        y.upload(f"{filename}", f"/backup/{filename}")
+        y.upload(f"{dump_dir}/{filename}", f"/backup/{filename}")
         print(f"Uploaded {filename}")
 
     else:
